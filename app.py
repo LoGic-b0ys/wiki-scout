0354d4333c1538f3ce3adfbd5ad8fcd401f60b64
Logic Boys <yanruziqa@gmail.com>
2019-05-29 12:10:39 +0700

    Add web view for article

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from Database import Database
 
-db = Database('article.db')
-
-row = db.read('articles', ['title'])
-
-print(row)
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+	db = Database('article.db')
+	row = db.read('articles', ['id', 'title'])
 	return render_template('index.html', data_title = row)
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+	db = Database('article.db')
+	data = db.read('articles', ['title', 'raw_article'], ' WHERE id='+str(article_id))
+	return render_template('article.html', article = data)
 
 Bootstrap(app)
 
